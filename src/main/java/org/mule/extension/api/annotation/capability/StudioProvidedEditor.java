9bb020798dfb3936ff1245e086e70ac47faa3f8f
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-03-03 12:42:34 -0300

    MULE-9440: Move extensions annotations to the extensions-api jar

---
+++ src/main/java/org/mule/extension/api/annotation/capability/StudioProvidedEditor.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.api.annotation.capability;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Signals that the UI for the mule extension will be provided by Anypoint Studio.
+ * <p/>
+ * Nothing will be derived from the mule extension code when using this annotation.
+ *
+ * @since 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface StudioProvidedEditor
+{
+
+}
