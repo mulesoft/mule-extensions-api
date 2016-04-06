4b6d872d718d8eacfaa4b9d3d2fdef003cd3ab23
alegmarra <alemarra@gmail.com>
2016-04-06 19:06:16 -0300

    Relocated SubTypesModelProperty

---
+++ mule-extensions-api/src/main/java/org/mule/extension/api/introspection/SubTypesModelProperty.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.api.introspection;
+
+
+import org.mule.metadata.api.model.MetadataType;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An immutable model property which specifies the relations of a given
+ * {@link MetadataType} and its declared subTypes, which are concrete implementations
+ * of the base {@link Class}
+ *
+ * @since 4.0
+ */
+public final class SubTypesModelProperty implements ModelProperty
+{
+
+    private final Map<MetadataType, List<MetadataType>> typesMap;
+
+    /**
+     * Creates a new instance containing all the baseType-subTypes declarations
+     * based on the {@link Class} references of each type
+     *
+     * @param mapping concrete implementations of the {@code baseType}
+     */
+    public SubTypesModelProperty(Map<MetadataType, List<MetadataType>> mapping)
+    {
+        this.typesMap = mapping;
+    }
+
+    /**
+     * @return the FIXME container which holds the relationships of types and subTypes
+     */
+    public Map<MetadataType, List<MetadataType>> getSubTypesMapping()
+    {
+        return Collections.unmodifiableMap(typesMap);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code subTypesMapping}
+     */
+    @Override
+    public String getName()
+    {
+        return "subTypesMapping";
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code true}
+     */
+    @Override
+    public boolean isExternalizable()
+    {
+        return true;
+    }
+}
