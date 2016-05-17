bb4d849ea554717b674aa6474d2703c32ef633af
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-05-17 17:43:09 -0300

    MULE-9604: Add support for package and resource export on Extensions

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/introspection/declaration/spi/Describer.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/introspection/declaration/spi/Describer.java
@@ -7,24 +7,13 @@
 package org.mule.runtime.extension.api.introspection.declaration.spi;
 
 
-import org.mule.runtime.extension.api.introspection.ExtensionModel;
 import org.mule.runtime.extension.api.introspection.declaration.DescribingContext;
 import org.mule.runtime.extension.api.introspection.declaration.fluent.ExtensionDeclarer;
 
 /**
- * Every Extension must ship with one implementation of this interface
- * which has to be discoverable through standard SPI and have a default constructor.
- * <p/>
- * This describer returns a {@link ExtensionDeclarer} which contains a raw representation of the
+ * Returns a {@link ExtensionDeclarer} which contains a raw representation of the
  * extension.
  * <p/>
- * The platform will discover all the describers, transform the returned {@link ExtensionDeclarer}s
- * into {@link ExtensionModel extensionModels} and register them.
- * <p/>
- * To allow customization of the describing process, implementations will use standard SPI discovery mechanism
- * to locale registered instances of {@link ModelEnricher}
- * which are invoked in order.
- *
  * @since 1.0
  */
 public interface Describer
