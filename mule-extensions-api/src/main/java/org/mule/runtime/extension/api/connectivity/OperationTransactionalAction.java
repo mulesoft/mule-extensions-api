7539530ec203a949b2cb68d9e57b8b992a80949b
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-07-13 12:51:40 -0300

    MULE-10063: Support single resource transactions on SDK operations

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/connectivity/OperationTransactionalAction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.connectivity;
+
+/**
+ * The type of joining actions that operations can take
+ * regarding transactions.
+ *
+ * @since 1.0
+ */
+public enum OperationTransactionalAction
+{
+    /**
+     * There must always be a transaction present for the invocation
+     * <p>
+     * J2EE: Mandatory
+     */
+    ALWAYS_JOIN,
+
+    /**
+     * If there is a transaction available, then use it, otherwise continue processing
+     * <p>
+     * J2EE: Supports
+     */
+    JOIN_IF_POSSIBLE,
+
+
+    /**
+     * Executes outside any existent transaction
+     */
+    NOT_SUPPORTED
+}
---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/connectivity/TransactionalConnection.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.connectivity;
+
+/**
+ * Connections which support transaction must implement this interface
+ * in order to start or join a current transaction.
+ *
+ * @since 1.0
+ */
+public interface TransactionalConnection extends Transactional
+{
+
+    /**
+     * @return whether the transaction is in XA mode
+     */
+    boolean isXA();
+}
