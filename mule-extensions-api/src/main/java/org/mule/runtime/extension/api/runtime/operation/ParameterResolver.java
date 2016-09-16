ab54f5603e0c7b446ecca1214acc650e5e5deb9e
estebanwasinger <estebanwasinger@gmail.com>
2016-09-16 19:22:02 -0300

    MULE-10339: Replace Function<MuleEvent, T> with a better abstraction

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/operation/ParameterResolver.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.runtime.operation;
+
+import java.util.Optional;
+
+/**
+ * A parameter type for operations that gives the capability of:
+ *
+ * <ul>
+ * <li><b>Get the configured expression:</b> With {@link #getExpression()} returns the possible
+ * expression used to resolve the parameter value
+ * </li>
+ * <li><b>Differ the resolution:</b> Using {@link #resolve()} resolves the parameter value of type
+ * {@link T}
+ * </ul>
+ * </p>
+ * This parameter type can only be used for operations.
+ *
+ * @param <T> The type of the value to resolve
+ * @since 1.0
+ */
+public interface ParameterResolver<T> {
+
+  /**
+   * @return Resolves the value of type {@link T} for the given parameter.
+   */
+  T resolve();
+
+  /**
+   * @return The optional expression that generates the required value. May be empty if the value
+   * was not generated from an expression
+   */
+  Optional<String> getExpression();
+}
