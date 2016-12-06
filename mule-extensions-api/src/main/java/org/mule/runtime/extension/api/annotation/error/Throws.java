3b9d719acf313bb97fc59255e14c25dee98e217a
estebanwasinger <estebanwasinger@gmail.com>
2016-12-06 18:56:08 -0300

    MULE-10271: Revamp validation module based on mule 4 error handling capabilities

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/annotation/error/Throws.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/annotation/error/Throws.java
@@ -7,8 +7,10 @@
 package org.mule.runtime.extension.api.annotation.error;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 
 import java.lang.annotation.Documented;
@@ -18,12 +20,17 @@ import java.lang.annotation.Target;
 /**
  * Declares an array of {@link Class classes} of an {@link ErrorTypeProvider}, to communicate and declare
  * which {@link ErrorTypeDefinition} the operation could throw.
+ * <p>
+ * This annotation can be applied to an Operation method, an Operation container class or an {@link Extension} class.
+ * The annotation value to consider is the one of in the deepest level,
+ * eg: If the annotation is used in an operation method and in an extension class, the one to use is the one in the
+ * operation.
  *
  * @since 1.0
  * @see ErrorTypeProvider
  * @see ErrorTypeDefinition
  */
-@Target({METHOD})
+@Target({METHOD, TYPE})
 @Retention(RUNTIME)
 @Documented
 public @interface Throws {
