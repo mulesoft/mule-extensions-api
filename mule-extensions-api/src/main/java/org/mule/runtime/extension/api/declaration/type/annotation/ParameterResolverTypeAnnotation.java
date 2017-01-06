a536409258ecac0f8b444224182af0ce16358df7
estebanwasinger <estebanwasinger@gmail.com>
2017-01-06 12:22:22 -0300

    MULE-11292: Functions, TypedValues and ParameterResolvers are not supported inside a Pojo

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/declaration/type/annotation/ParameterResolverTypeAnnotation.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.declaration.type.annotation;
+
+import org.mule.metadata.api.annotation.TypeAnnotation;
+import org.mule.runtime.extension.api.runtime.operation.ParameterResolver;
+
+/**
+ * {@link TypeAnnotation} indicating that the real type of the of the annotated type is a {@link ParameterResolver}
+ *
+ * @since 1.0
+ * @see ParameterResolver
+ */
+public class ParameterResolverTypeAnnotation implements TypeAnnotation {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getName() {
+    return "parameterResolver";
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean equals(Object obj) {
+    return obj instanceof ParameterResolverTypeAnnotation;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int hashCode() {
+    return 1681352;
+  }
+}
