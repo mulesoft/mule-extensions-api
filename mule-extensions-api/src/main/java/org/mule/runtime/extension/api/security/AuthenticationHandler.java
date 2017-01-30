9c551611acdd99d0fe57536a9e78cbeee757a814
alegmarra <alemarra@gmail.com>
2017-01-30 14:05:44 -0300

    MULE-11440: Provide a way to set the SecurityContext in SDK

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/security/AuthenticationHandler.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.security;
+
+import org.mule.runtime.api.security.Authentication;
+import org.mule.runtime.api.security.Credentials;
+import org.mule.runtime.api.security.CredentialsBuilder;
+import org.mule.runtime.api.security.SecurityException;
+import org.mule.runtime.api.security.SecurityProviderNotFoundException;
+import org.mule.runtime.api.security.UnknownAuthenticationTypeException;
+
+import java.util.List;
+
+/**
+ * This handler allows to configure the current context's authentication, used for encryption and inbound authentication,
+ * based on a given {@link Authentication} request.
+ *
+ * @since 1.0
+ */
+public interface AuthenticationHandler {
+
+  /**
+   * Updates the current context with the given {@link Authentication} information.
+   *
+   * @param authentication the {@link Authentication} used for setting up the new security context
+   *
+   * @throws SecurityProviderNotFoundException if no security provider able to authenticate the given authentication is found
+   * @throws SecurityException if an error occurs during the update of the security context
+   * @throws UnknownAuthenticationTypeException if no security provider able to handle the given authentication is found
+   */
+  void setAuthentication(Authentication authentication)
+      throws SecurityProviderNotFoundException, SecurityException, UnknownAuthenticationTypeException;
+
+  /**
+   * Updates the current context with the given {@link Authentication} information.
+   *
+   * @param securityProviders the {@link List} of security providers that will be added to the {@code SecurityManager}
+   * @param authentication the {@link Authentication} used for setting up the new security context
+   *
+   * @throws SecurityProviderNotFoundException if no security provider able to authenticate the given authentication is found
+   * @throws SecurityException if an error occurs during the update of the security context
+   * @throws UnknownAuthenticationTypeException if no security provider able to handle the given authentication is found
+   */
+  void setAuthentication(List<String> securityProviders, Authentication authentication)
+      throws SecurityProviderNotFoundException, SecurityException, UnknownAuthenticationTypeException;
+
+  /**
+   * @return the {@link Authentication} in the current context
+   */
+  Authentication getAuthentication();
+
+  /**
+   * @param credentials the {@link Credentials} to be used for {@code this} {@link Authentication}
+   * @return a new instance of a default implementation of {@link Authentication}
+   */
+  Authentication createDefaultAuthentication(Credentials credentials);
+
+  /**
+   * @return a new {@link CredentialsBuilder}
+   */
+  CredentialsBuilder createCredentials();
+
+}
