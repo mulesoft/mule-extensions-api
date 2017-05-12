820a1eeb2ba60a453464305491a0ca576cb6f9fd
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-05-12 15:02:55 -0300

    MULE-12459: OAuth extension fails if it doesn't provide default set of scopes

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/connectivity/oauth/AuthorizationCodeGrantType.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/connectivity/oauth/AuthorizationCodeGrantType.java
@@ -6,6 +6,8 @@
  */
 package org.mule.runtime.extension.api.connectivity.oauth;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 /**
  * Implementaiton of {@Link OAuthGrantType} which contains inforamtion about how to use the
  * Authorization-Code grant type against a particular OAuth provider
@@ -40,14 +42,13 @@ public final class AuthorizationCodeGrantType implements OAuthGrantType {
     notBlank(accessTokenExpr, "accessTokenExpr");
     notBlank(expirationRegex, "expirationRegex");
     notBlank(expirationRegex, "expirationRegex");
-    notBlank(defaultScope, "defaultScope");
 
     this.accessTokenUrl = accessTokenUrl;
     this.authorizationUrl = authorizationUrl;
     this.accessTokenExpr = accessTokenExpr;
     this.expirationRegex = expirationRegex;
     this.refreshTokenExpr = refreshTokenExpr;
-    this.defaultScope = defaultScope;
+    this.defaultScope = isBlank(defaultScope) ? null : defaultScope;
   }
 
   private void notBlank(String value, String name) {
@@ -100,7 +101,7 @@ public final class AuthorizationCodeGrantType implements OAuthGrantType {
   }
 
   /**
-   * @return The default scopes to be request
+   * @return The default scopes to be request or a blank string if no default scopes are required.
    */
   public String getDefaultScope() {
     return defaultScope;
