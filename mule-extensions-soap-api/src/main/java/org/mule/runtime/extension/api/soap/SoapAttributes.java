5ca8b4a3cb86147ca51ccb055c7f82efc86e8568
JuanDesi <juan.desimoni@mulesoft.com>
2017-08-04 19:32:44 -0300

    SQUASH!

---
+++ mule-extensions-soap-api/src/main/java/org/mule/runtime/extension/api/soap/SoapAttributes.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.soap;
+
+import static com.google.common.collect.ImmutableMap.copyOf;
+import static com.google.common.collect.ImmutableMap.of;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * Contains the headers retrieved by the protocol after the request.
+ *
+ * @since 4.0
+ */
+public final class SoapAttributes implements Serializable {
+
+  private final Map<String, String> protocolHeaders;
+
+  public SoapAttributes(Map<String, String> protocolHeaders) {
+    this.protocolHeaders = protocolHeaders != null ? copyOf(protocolHeaders) : of();
+  }
+
+  /**
+   * @return a set of protocol headers bounded to the service response. i.e. HTTP Headers.
+   */
+  public Map<String, String> getProtocolHeaders() {
+    return protocolHeaders;
+  }
+}
