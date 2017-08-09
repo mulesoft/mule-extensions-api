a54610b1551296e4458088d38053ee9fdf674a39
afelisatti <afelisatti@mulesoft.com>
2017-08-09 17:54:13 -0300

    MULE-13290: Extension model is missing types and extensions declare own any

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/error/MuleErrors.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/error/MuleErrors.java
@@ -18,14 +18,75 @@ import java.util.Optional;
  */
 public enum MuleErrors implements ErrorTypeDefinition<MuleErrors> {
 
-  ANY, CONNECTIVITY(ANY), TRANSFORMATION(ANY), EXPRESSION(ANY), REDELIVERY_EXHAUSTED(ANY), RETRY_EXHAUSTED(ANY), ROUTING(
-      ANY), SECURITY(ANY), CLIENT_SECURITY(SECURITY), SERVER_SECURITY(SECURITY), OVERLOAD(ANY),
-
+  /**
+   * Wild card that matches with any error and is on top of the error hierarchy for those that allow handling.
+   */
+  ANY,
+  /**
+   * Indicates that a problem occurred and a connection could not be established.
+   */
+  CONNECTIVITY(ANY),
+  /**
+   * Indicates that a problem occurred when transforming a value.
+   */
+  TRANSFORMATION(ANY),
+  /**
+   * Indicates that a problem occurred when resolving an expression.
+   */
+  EXPRESSION(ANY),
+  /**
+   * Indicates that the retry policy, of a certain component, to execute some action, eg: connectivity, delivery has been.
+   * exhausted
+   */
+  REDELIVERY_EXHAUSTED(ANY),
+  /**
+   * Indicates that the retry of a certain execution block has been exhausted.
+   */
+  RETRY_EXHAUSTED(ANY),
+  /**
+   * Indicates that a problem occurred when routing a message.
+   */
+  ROUTING(ANY),
+  /**
+   * Indicates a security type problem occurred, eg: invalid credentials, expired token, etc.
+   */
+  SECURITY(ANY),
+  /**
+   * Indicates a security type problem enforced by an external entity.
+   */
+  CLIENT_SECURITY(SECURITY),
+  /**
+   * Indicates a security type problem enforced by the mule runtime.
+   */
+  SERVER_SECURITY(SECURITY),
+  /**
+   * Indicates that an error occurred in the source of a flow.
+   */
   SOURCE(ANY),
-
-  SOURCE_ERROR_RESPONSE_GENERATE(SOURCE), SOURCE_ERROR_RESPONSE_SEND(SOURCE),
-
-  SOURCE_RESPONSE(SOURCE), SOURCE_RESPONSE_GENERATE(SOURCE_RESPONSE), SOURCE_RESPONSE_SEND(SOURCE_RESPONSE);
+  /**
+   * Indicates that an error occurred in the source of the flow processing a successful response.
+   */
+  SOURCE_RESPONSE(SOURCE),
+  /**
+   * Indicates that an error occurred in the source of the flow generating the parameters of a successful response.
+   */
+  SOURCE_RESPONSE_GENERATE(SOURCE_RESPONSE),
+  /**
+   * Indicates that an error occurred in the source of the flow sending a successful response.
+   */
+  SOURCE_RESPONSE_SEND(SOURCE_RESPONSE),
+  /**
+   * Indicates that an error occurred in the source of the flow generating the parameters of an error response.
+   */
+  SOURCE_ERROR_RESPONSE_GENERATE(SOURCE),
+  /**
+   * Indicates that an error occurred in the source of the flow sending an error response.
+   */
+  SOURCE_ERROR_RESPONSE_SEND(SOURCE),
+  /**
+   * Indicates that a severe error occurred. Cannot be handled. Top of the error hierarchy for those that do not allow handling.
+   */
+  CRITICAL;
 
   private ErrorTypeDefinition<MuleErrors> parentError;
 
