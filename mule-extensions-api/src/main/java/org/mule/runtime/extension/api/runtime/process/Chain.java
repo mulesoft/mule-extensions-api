f1c9b0723c294e13ae3163f456f90367259f2bf6
alegmarra <alemarra@gmail.com>
2017-09-02 13:05:21 -0300

    chain jdoc

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/process/Chain.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/process/Chain.java
@@ -6,33 +6,73 @@
  */
 package org.mule.runtime.extension.api.runtime.process;
 
-import org.mule.runtime.api.meta.model.ComponentModel;
-import org.mule.runtime.api.meta.model.ComposableModel;
-import org.mule.runtime.api.meta.model.nested.NestedChainModel;
+import org.mule.runtime.api.message.Message;
+import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 /**
- * The executable component associated to a {@link NestedChainModel}.
- * For a given {@link ComposableModel} to be able to receive a chain of nested processor and execute it,
- * it must receive a {@link Chain}.
- * This processor chain will be represented in the owner's {@link ComponentModel} by a {@link NestedChainModel}.
+ * A {@link Chain} allows a given {@link OperationModel Operation} to receive a chain of message processors
+ * to be executed as part of the operation's execution.
+ * <p>
+ * When executing the {@link Chain}, all it's inner components will be executed in the order declared by the
+ * user, using the output of one processor as the input of the next one in the {@link Chain}. The execution
+ * of the {@link Chain} is completed either when all the components were invoked successfully or when an error
+ * occurs in one processor and it is propagated to the {@link Chain}
  *
  * @since 1.0
  */
 public interface Chain {
 
   /**
-   *   
-   * @param onSuccess
-   * @param onError
+   * Executes the chain of components starting with the same input message that it's container scope received.
+   * <p>
+   * {@code onSuccess} callback will be invoked with the output {@link Result} of the last component
+   * in the {@link Chain} if no error occurred after all the components were executed exactly once.
+   * <p>
+   * {@code onError} callback will be invoked with the exception propagated by the first failing component,
+   * along with the last output {@link Result} available. The given {@link Result} will be the same that was
+   * used as input of the failing component.
+   *
+   * @param onSuccess the callback to be executed when a successful execution is completed by the {@link Chain}
+   * @param onError  the callback to be executed when an error occurs during the execution
+   *                 of the {@link Chain} components
    */
   void process(Consumer<Result> onSuccess, BiConsumer<Throwable, Result> onError);
 
+  /**
+   * Executes the chain of components starting with the given {@code payload} and {@code attributes} as {@link Message}.
+   * <p>
+   * {@code onSuccess} callback will be invoked with the output {@link Result} of the last component
+   * in the {@link Chain} if no error occurred after all the components were executed exactly once.
+   * <p>
+   * {@code onError} callback will be invoked with the exception propagated by the first failing component,
+   * along with the last output {@link Result} available. The given {@link Result} will be the same that was
+   * used as input of the failing component.
+   *
+   * @param onSuccess the callback to be executed when a successful execution is completed by the {@link Chain}
+   * @param onError  the callback to be executed when an error occurs during the execution
+   *                 of the {@link Chain} components
+   */
   void process(Object payload, Object attributes, Consumer<Result> onSuccess, BiConsumer<Throwable, Result> onError);
 
+  /**
+   * Executes the chain of components starting with the given {@link Result} data as input {@link Message}.
+   * This method should be used whenever the input of this {@link Chain} is the output of another component execution.
+   * <p>
+   * {@code onSuccess} callback will be invoked with the output {@link Result} of the last component
+   * in the {@link Chain} if no error occurred after all the components were executed exactly once.
+   * <p>
+   * {@code onError} callback will be invoked with the exception propagated by the first failing component,
+   * along with the last output {@link Result} available. The given {@link Result} will be the same that was
+   * used as input of the failing component.
+   *
+   * @param onSuccess the callback to be executed when a successful execution is completed by the {@link Chain}
+   * @param onError  the callback to be executed when an error occurs during the execution
+   *                 of the {@link Chain} components
+   */
   void process(Result input, Consumer<Result> onSuccess, BiConsumer<Throwable, Result> onError);
 
 }
