7068dd237951933e93df8b08412c520cf3c1a13e
JuanDesi <juan.desimoni@mulesoft.com>
2017-09-04 16:16:26 -0300

    MULE-13420: Cannot test metadata on SOAP connectors

---
+++ mule-extensions-soap-api/src/main/java/org/mule/runtime/extension/api/soap/WebServiceTypeKey.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.soap;
+
+/**
+ * Represents a multilevel metadata key that describes a soap operation giving a web service.
+ *
+ * @since 1.0
+ */
+public final class WebServiceTypeKey {
+
+  /**
+   * The service that contains the operation
+   */
+  private String service;
+
+  /**
+   * The operation to be executed
+   */
+  private String operation;
+
+  public String getService() {
+    return service;
+  }
+
+  public String getOperation() {
+    return operation;
+  }
+}
