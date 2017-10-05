1accdb631648937d2de70bab3d73a4e92e7c9bf0
fernandezlautaro <fernandez.lautaro@gmail.com>
2017-10-05 14:09:09 -0300

    MULE-13713: reconnection element it is not supported for SC

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/loader/enricher/ReconnectionStrategyDeclarationEnricher.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/loader/enricher/ReconnectionStrategyDeclarationEnricher.java
@@ -7,11 +7,13 @@
 package org.mule.runtime.extension.internal.loader.enricher;
 
 import static org.mule.runtime.extension.internal.loader.util.InfrastructureParameterBuilder.addReconnectionStrategyParameter;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.OperationDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
 import org.mule.runtime.extension.api.declaration.fluent.util.IdempotentDeclarationWalker;
 import org.mule.runtime.extension.api.loader.DeclarationEnricher;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
+import org.mule.runtime.extension.internal.property.NoReconnectionStrategyModelProperty;
 
 /**
  * A {@link DeclarationEnricher} which adds the following to all {@link SourceDeclaration}:
@@ -26,19 +28,23 @@ public final class ReconnectionStrategyDeclarationEnricher implements Declaratio
 
   @Override
   public void enrich(ExtensionLoadingContext extensionLoadingContext) {
-    new IdempotentDeclarationWalker() {
+    final ExtensionDeclaration declaration = extensionLoadingContext.getExtensionDeclarer().getDeclaration();
+    if (!declaration.getModelProperty(NoReconnectionStrategyModelProperty.class).isPresent()) {
 
-      @Override
-      protected void onSource(SourceDeclaration declaration) {
-        addReconnectionStrategyParameter(declaration);
-      }
+      new IdempotentDeclarationWalker() {
 
-      @Override
-      protected void onOperation(OperationDeclaration declaration) {
-        if (declaration.isRequiresConnection()) {
+        @Override
+        protected void onSource(SourceDeclaration declaration) {
           addReconnectionStrategyParameter(declaration);
         }
-      }
-    }.walk(extensionLoadingContext.getExtensionDeclarer().getDeclaration());
+
+        @Override
+        protected void onOperation(OperationDeclaration declaration) {
+          if (declaration.isRequiresConnection()) {
+            addReconnectionStrategyParameter(declaration);
+          }
+        }
+      }.walk(declaration);
+    }
   }
 }
