f0bbbd2f0a5eb634bbf3c4a4acc74cf5a80cefff
Juan Desimoni <juan.desimoni@mulesoft.com>
2018-01-19 15:30:33 -0300

    MULE-14388: Extensions Client must use the correct event when used in an operation (#555)
    * MULE-14388: Extensions Client must use the correct event when used in an operation
    
    * Update MessageDispatcherProvider.java

---
+++ mule-extensions-soap-api/src/main/java/org/mule/runtime/extension/api/soap/DispatchingContext.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.runtime.extension.api.soap;
+
+import org.mule.runtime.extension.api.client.ExtensionsClient;
+import org.mule.runtime.extension.api.soap.message.MessageDispatcher;
+
+/**
+ * A resolving context that provides access to an {@link ExtensionsClient} during the creation of a {@link MessageDispatcher}.
+ *
+ * @since 4.1
+ */
+public interface DispatchingContext {
+
+  /**
+   * @return a well initialized extensions client.
+   */
+  ExtensionsClient getExtensionsClient();
+}
