49df1dee124a0924bdb534318ee92faec85484ae
Juan Desimoni <juan.desimoni@mulesoft.com>
2018-01-24 16:48:42 -0300

    MULE-13025: Enable Test Connetivity on SOAP connectors (#561) (#562)

--- mule-extensions-soap-api/src/main/java/org/mule/runtime/extension/api/soap/MessageDispatcherProvider.java
+++ mule-extensions-soap-api/src/main/java/org/mule/runtime/extension/api/soap/MessageDispatcherProvider.java
@@ -7,6 +7,7 @@
 package org.mule.runtime.extension.api.soap;
 
 import org.mule.runtime.api.connection.ConnectionProvider;
+import org.mule.runtime.api.connection.ConnectionValidationResult;
 import org.mule.runtime.extension.api.soap.message.MessageDispatcher;
 
 /**
@@ -17,4 +18,31 @@ import org.mule.runtime.extension.api.soap.message.MessageDispatcher;
  */
 public interface MessageDispatcherProvider<T extends MessageDispatcher> extends ConnectionProvider<T> {
 
+  /**
+   * Validates the given {@link T}.
+   *
+   * In invalid connection case, the {@link ConnectionValidationResult} should also return a valid
+   * message {@link ConnectionValidationResult#getMessage()}, exception {@link ConnectionValidationResult#getException()}
+   * and code {@link ConnectionValidationResult#getErrorType()}
+   *
+   * @param connection a non {@code null} {@link T}.
+   * @return a {@link ConnectionValidationResult} indicating if the connection
+   * is valid or not.
+   *
+   * @since 1.1
+   */
+  default ConnectionValidationResult validate(T connection, SoapServiceProvider provider) {
+    return ConnectionValidationResult.success();
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @deprecated use {@link MessageDispatcherProvider#validate(MessageDispatcher, SoapServiceProvider)}
+   */
+  @Deprecated
+  @Override
+  default ConnectionValidationResult validate(T connection) {
+    return ConnectionValidationResult.success();
+  }
 }
