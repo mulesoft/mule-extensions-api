6ef8f903362cf74e8d8d86ee8dfcac0b826396a9
alegmarra <alemarra@gmail.com>
2018-06-15 20:00:32 -0300

    MULE-15162: Operations Stereotypes don't match the @AllowedStereotypes definition

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/stereotype/MuleStereotypeDefinition.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/stereotype/MuleStereotypeDefinition.java
@@ -6,11 +6,14 @@
  */
 package org.mule.runtime.extension.api.stereotype;
 
+import org.mule.api.annotation.NoExtend;
+
 /**
  * Marker interface that identifies a given {@link StereotypeDefinition} as a Mule stereotype.
  *
  * @since 1.0
  */
+@NoExtend
 public abstract class MuleStereotypeDefinition implements StereotypeDefinition {
 
   public static final String NAMESPACE = "MULE";
