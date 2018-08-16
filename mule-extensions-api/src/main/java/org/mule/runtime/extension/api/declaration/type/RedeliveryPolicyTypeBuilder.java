d9f1c84f70655267f74fe2fa714b31794ebf9a09
bardammu <juan.baca@mulesoft.com>
2018-08-16 15:19:14 -0300

    MULE-15163: Change redelivery policy model to declare exclusiveOptional fields with one-required

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/declaration/type/RedeliveryPolicyTypeBuilder.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/declaration/type/RedeliveryPolicyTypeBuilder.java
@@ -6,14 +6,18 @@
  */
 package org.mule.runtime.extension.api.declaration.type;
 
+import static com.google.common.collect.Sets.newHashSet;
 import static java.util.Collections.singletonList;
 import static org.mule.metadata.api.builder.BaseTypeBuilder.create;
 import static org.mule.metadata.api.model.MetadataFormat.JAVA;
 import static org.mule.runtime.extension.api.stereotype.MuleStereotypes.OBJECT_STORE;
+
 import org.mule.metadata.api.builder.BaseTypeBuilder;
 import org.mule.metadata.api.builder.ObjectTypeBuilder;
 import org.mule.metadata.api.model.MetadataType;
 import org.mule.runtime.api.store.ObjectStore;
+import org.mule.runtime.extension.api.declaration.type.annotation.ExclusiveOptionalsTypeAnnotation;
+import org.mule.runtime.extension.api.declaration.type.annotation.FlattenedTypeAnnotation;
 import org.mule.runtime.extension.api.declaration.type.annotation.InfrastructureTypeAnnotation;
 import org.mule.runtime.extension.api.declaration.type.annotation.StereotypeTypeAnnotation;
 
@@ -30,6 +34,7 @@ public final class RedeliveryPolicyTypeBuilder extends InfrastructureTypeBuilder
   public static final String ID_EXPRESSION = "idExpression";
   public static final String OBJECT_STORE_REF = "objectStore";
   public static final String REDELIVERY_POLICY = "RedeliveryPolicy";
+  public static final String MESSAGE_IDENTIFIER = "MessageIdentifier";
 
   /**
    * @return a {@link MetadataType} representation of a redelivery policy
@@ -47,20 +52,16 @@ public final class RedeliveryPolicyTypeBuilder extends InfrastructureTypeBuilder
                     + "process-failed-message",
                 5);
 
-    addBooleanField(objectType, typeBuilder,
-                    USE_SECURE_HASH,
-                    "Whether to use a secure hash algorithm to identify a redelivered message",
-                    true);
     addStringField(objectType, typeBuilder,
                    MESSAGE_DIGEST_ALGORITHM,
                    "The secure hashing algorithm to use. If not set, the default is SHA-256.",
                    null);
 
-    addStringField(objectType, typeBuilder,
-                   ID_EXPRESSION,
-                   "Defines one or more expressions to use to determine when a message has been redelivered. "
-                       + "This property may only be set if useSecureHash is false.",
-                   null);
+    objectType.addField()
+        .with(new FlattenedTypeAnnotation())
+        .description("Defines which strategy is used to identify the messages.")
+        .key(MESSAGE_IDENTIFIER)
+        .value(buildMessageFilterType());
 
     objectType.addField()
         .with(new StereotypeTypeAnnotation(singletonList(OBJECT_STORE)))
@@ -68,7 +69,27 @@ public final class RedeliveryPolicyTypeBuilder extends InfrastructureTypeBuilder
         .key(OBJECT_STORE_REF)
         .value(ExtensionsTypeLoaderFactory.getDefault().createTypeLoader().load(ObjectStore.class));
 
-
     return objectType.build();
   }
+
+  private MetadataType buildMessageFilterType() {
+    final ObjectTypeBuilder objectTypeBuilder = create(JAVA).objectType().id("RedeliveryPolicyMessageIdentifier")
+        .with(new ExclusiveOptionalsTypeAnnotation(newHashSet(USE_SECURE_HASH, ID_EXPRESSION), true));
+
+    BaseTypeBuilder typeBuilder = create(JAVA);
+
+    addBooleanField(objectTypeBuilder, typeBuilder,
+                    USE_SECURE_HASH,
+                    "Whether to use a secure hash algorithm to identify a redelivered message",
+                    true);
+
+    addStringField(objectTypeBuilder, typeBuilder,
+                   ID_EXPRESSION,
+                   "Defines one or more expressions to use to determine when a message has been redelivered. "
+                       + "This property may only be set if useSecureHash is false.",
+                   null);
+
+    return objectTypeBuilder.build();
+  }
+
 }
