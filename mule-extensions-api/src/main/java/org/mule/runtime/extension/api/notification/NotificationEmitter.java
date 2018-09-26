3a81db0bafda2b3716db25728e57af6a30f4803e
Rodrigo Merino <elrodro83@users.noreply.github.com>
2018-09-26 08:50:35 -0300

    MULE-15740: NotificationEmitter: support lazy creation of notification (#604)

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/notification/NotificationEmitter.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/notification/NotificationEmitter.java
@@ -7,8 +7,11 @@
 package org.mule.runtime.extension.api.notification;
 
 import org.mule.api.annotation.NoImplement;
+import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.api.metadata.TypedValue;
 import org.mule.runtime.api.notification.ExtensionNotification;
+
+import java.util.function.Supplier;
 
 /**
  * This component allows firing {@link ExtensionNotification ExtensionNotifications} with both custom data and
@@ -26,5 +29,18 @@ public interface NotificationEmitter {
    * @param data the {@link TypedValue} data to use.
    */
   void fire(NotificationActionDefinition action, TypedValue<?> data);
+
+  /**
+   * Fires an {@link ExtensionNotification} with the desired information.
+   *
+   * @param action the {@link NotificationActionDefinition} to use.
+   * @param dataValue a supplier for the the {@link Object} to use as value of the generated {@link DataType}.
+   * @param dataType the type of the data returned by the provided supplier.
+   *
+   * @since 4.2.0
+   */
+  default void fireLazy(NotificationActionDefinition action, Supplier<?> dataValue, DataType dataType) {
+    fire(action, new TypedValue<>(dataValue.get(), dataType));
+  }
 
 }
