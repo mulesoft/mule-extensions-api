34e37905e8ca8c1186a3933d3f09ff4be097e2a3
Alejandro García Marra <alemarra@gmail.com>
2019-04-08 18:25:04 -0300

    MULE-16644: PollingSources ObjectStore names need to be part of the API (#633)

---
+++ mule-extensions-api/src/test/java/org/mule/runtime/extension/api/runtime/PollingSourceTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.runtime;
+
+import static java.lang.String.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mule.runtime.extension.api.runtime.source.PollingSource.IDS_ON_UPDATED_WATERMARK_OS_NAME_SUFFIX;
+import static org.mule.runtime.extension.api.runtime.source.PollingSource.OS_NAME_MASK;
+import static org.mule.runtime.extension.api.runtime.source.PollingSource.RECENTLY_PROCESSED_IDS_OS_NAME_SUFFIX;
+import static org.mule.runtime.extension.api.runtime.source.PollingSource.WATERMARK_OS_NAME_SUFFIX;
+
+import org.junit.Test;
+
+public class PollingSourceTestCase {
+
+  public static final String TEST_FLOW_NAME = "myFlow";
+  public static final String EXPECTED_WATERMARK_OS = "_pollingSource_myFlow/watermark";
+  public static final String EXPECTED_RECENT_IDS_OS = "_pollingSource_myFlow/recently-processed-ids";
+  public static final String EXPECTED_IDS_UPDATED_WATERMARK_OS = "_pollingSource_myFlow/ids-on-updated-watermark";
+
+  @Test
+  public void watermarkObjectStoreNameIsBackwardsCompatible() {
+    final String watermarkOs = format(OS_NAME_MASK, TEST_FLOW_NAME, WATERMARK_OS_NAME_SUFFIX);
+    assertThat(watermarkOs, is(EXPECTED_WATERMARK_OS));
+  }
+
+  @Test
+  public void recentlyProcessedIdsObjectStoreNameIsBackwardsCompatible() {
+    final String watermarkOs = format(OS_NAME_MASK, TEST_FLOW_NAME, RECENTLY_PROCESSED_IDS_OS_NAME_SUFFIX);
+    assertThat(watermarkOs, is(EXPECTED_RECENT_IDS_OS));
+  }
+
+  @Test
+  public void idsUpdatedWatermarkObjectStoreNameIsBackwardsCompatible() {
+    final String watermarkOs = format(OS_NAME_MASK, TEST_FLOW_NAME, IDS_ON_UPDATED_WATERMARK_OS_NAME_SUFFIX);
+    assertThat(watermarkOs, is(EXPECTED_IDS_UPDATED_WATERMARK_OS));
+  }
+}
