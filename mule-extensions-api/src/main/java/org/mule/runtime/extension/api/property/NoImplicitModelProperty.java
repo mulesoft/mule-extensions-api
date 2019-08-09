068b52196d8ee53790e4b020e7960e3fd43c6522
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-08-09 18:28:19 -0300

    self review

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/property/NoImplicitModelProperty.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/property/NoImplicitModelProperty.java
@@ -8,6 +8,11 @@ package org.mule.runtime.extension.api.property;
 
 import org.mule.runtime.api.meta.model.ModelProperty;
 
+/**
+ * Model property that indicates that the enriched model should not be used in an implicit way
+ *
+ * @since 1.3.0
+ */
 public class NoImplicitModelProperty implements ModelProperty {
 
   @Override
