42243ce0ada140860ef2c8266a53d79100f59ec8
Ezequiel Werner <eze210@gmail.com>
2020-07-30 12:16:26 -0300

    feedback

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/config/ExpirableConfigurationProvider.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/config/ExpirableConfigurationProvider.java
@@ -17,7 +17,7 @@ import java.util.List;
  * when it tries to locate expired configurations which need disposal
  *
  * @since 1.0
- * @deprecated the expirable configuration providers must handle expirations internally by
+ * @deprecated since 1.4.0, the expirable configuration providers must handle expirations internally by
  * calling {@link ExtensionManager#disposeConfiguration(String key, ConfigurationInstance configuration)}.
  */
 @NoImplement
