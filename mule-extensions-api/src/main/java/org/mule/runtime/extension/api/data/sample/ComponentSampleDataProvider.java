41fe552332f9f6fdacda81a34db9ac6af90547a0
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-09-11 13:35:59 -0300

    feedback

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/data/sample/ComponentSampleDataProvider.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/data/sample/ComponentSampleDataProvider.java
@@ -10,8 +10,19 @@ import org.mule.api.annotation.NoImplement;
 import org.mule.runtime.api.message.Message;
 import org.mule.sdk.api.data.sample.SampleDataException;
 
+/**
+ * Contract for components capable of providing sample data.
+ *
+ * @since 1.4.0
+ */
 @NoImplement
 public interface ComponentSampleDataProvider {
 
+  /**
+   * Retrieves sample data based on the component's current configuration
+   *
+   * @return a {@link Message} carrying sample data
+   * @throws SampleDataException if resolution fails
+   */
   Message getSampleData() throws SampleDataException;
 }
