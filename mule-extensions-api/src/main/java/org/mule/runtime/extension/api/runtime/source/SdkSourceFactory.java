0b336cd278ab08c6613a7497785e681caf75222f
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2020-12-18 12:18:41 -0300

    progress

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/source/SdkSourceFactory.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/runtime/source/SdkSourceFactory.java
@@ -7,9 +7,10 @@
 package org.mule.runtime.extension.api.runtime.source;
 
 import org.mule.api.annotation.NoImplement;
+import org.mule.runtime.api.functional.Either;
 
 /**
- * Factory object for instances of {@link org.mule.sdk.api.runtime.source.Source}.
+ * Factory object for instances of either {@link org.mule.sdk.api.runtime.source.Source} or {@link Source}.
  * <p>
  * Implementations are to be reusable and thread-safe
  *
@@ -19,9 +20,9 @@ import org.mule.api.annotation.NoImplement;
 public interface SdkSourceFactory {
 
   /**
-   * Creates a new instance of {@link org.mule.sdk.api.runtime.source.Source}
+   * Creates a new instance of either {@link org.mule.sdk.api.runtime.source.Source} or {@link Source}.
    *
-   * @return a new {@link org.mule.sdk.api.runtime.source.Source}
+   * @return a new {@link org.mule.sdk.api.runtime.source.Source} or {@link Source}.
    */
-  org.mule.sdk.api.runtime.source.Source createMessageSource();
+  Either<org.mule.sdk.api.runtime.source.Source, Source> createMessageSource();
 }
