52126fab61af5c7f7df3904f0f59556e8bbc1cec
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2021-01-04 15:50:10 -0300

    progress

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/declaration/type/ExtensionObjectTypeHandler.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/declaration/type/ExtensionObjectTypeHandler.java
@@ -38,6 +38,8 @@ import org.mule.runtime.extension.api.runtime.parameter.Literal;
 import org.mule.runtime.extension.api.runtime.parameter.ParameterResolver;
 import org.mule.runtime.extension.api.stereotype.ImplicitStereotypeDefinition;
 import org.mule.runtime.extension.api.stereotype.StereotypeDefinition;
+import org.mule.sdk.api.runtime.parameter.Literal;
+import org.mule.sdk.api.runtime.parameter.ParameterResolver;
 
 import java.lang.reflect.Type;
 import java.util.List;
