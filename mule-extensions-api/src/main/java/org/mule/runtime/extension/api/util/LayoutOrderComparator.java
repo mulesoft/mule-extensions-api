f062b36ca7e758d7c8b107b3ca95cb0a916be0e0
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-06-10 13:34:32 -0300

    MULE-19168: Addendum, fix parameter order conssistency with preexsiting xml schema(#797)

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/util/LayoutOrderComparator.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/util/LayoutOrderComparator.java
@@ -7,6 +7,8 @@
 package org.mule.runtime.extension.api.util;
 
 import org.mule.metadata.api.model.ObjectFieldType;
+import org.mule.runtime.api.meta.model.display.LayoutModel;
+import org.mule.runtime.api.meta.model.parameter.ParameterModel;
 import org.mule.runtime.extension.api.declaration.type.annotation.LayoutTypeAnnotation;
 
 import java.util.Comparator;
@@ -16,18 +18,38 @@ import java.util.Comparator;
  *
  * @since 1.4
  */
-public class LayoutOrderComparator implements Comparator<ObjectFieldType> {
+public class LayoutOrderComparator {
 
-  public static final LayoutOrderComparator BY_LAYOUT_ORDER = new LayoutOrderComparator();
+  public static final Comparator<ObjectFieldType> OBJECTS_FIELDS_BY_LAYOUT_ORDER = new LayoutOrderObjectFieldsComparator();
+  public static final Comparator<ParameterModel> PARAMETER_MODEL_BY_LAYOUT_ORDER = new LayoutOrderParameterModelComparator();
 
   private LayoutOrderComparator() {
     // Nothing to do
   }
 
-  @Override
-  public int compare(ObjectFieldType o1, ObjectFieldType o2) {
-    return o1.getAnnotation(LayoutTypeAnnotation.class).flatMap(LayoutTypeAnnotation::getOrder).orElse(-1)
-        - o2.getAnnotation(LayoutTypeAnnotation.class).flatMap(LayoutTypeAnnotation::getOrder).orElse(-1);
+  private static class LayoutOrderObjectFieldsComparator implements Comparator<ObjectFieldType> {
+
+    private LayoutOrderObjectFieldsComparator() {
+      // Nothing to do
+    }
+
+    @Override
+    public int compare(ObjectFieldType o1, ObjectFieldType o2) {
+      return o1.getAnnotation(LayoutTypeAnnotation.class).flatMap(LayoutTypeAnnotation::getOrder).orElse(-1)
+          - o2.getAnnotation(LayoutTypeAnnotation.class).flatMap(LayoutTypeAnnotation::getOrder).orElse(-1);
+    }
   }
 
+  private static class LayoutOrderParameterModelComparator implements Comparator<ParameterModel> {
+
+    private LayoutOrderParameterModelComparator() {
+      // Nothing to do
+    }
+
+    @Override
+    public int compare(ParameterModel p1, ParameterModel p2) {
+      return p1.getLayoutModel().flatMap(LayoutModel::getOrder).orElse(-1)
+          - p2.getLayoutModel().flatMap(LayoutModel::getOrder).orElse(-1);
+    }
+  }
 }
