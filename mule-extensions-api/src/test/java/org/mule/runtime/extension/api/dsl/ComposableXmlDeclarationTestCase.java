41e735ae9dc4197b5e99a65e339b879049debff7
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2021-07-23 12:43:54 -0300

    more progress

--- mule-extensions-api/src/test/java/org/mule/runtime/extension/api/dsl/ComposableXmlDeclarationTestCase.java
+++ mule-extensions-api/src/test/java/org/mule/runtime/extension/api/dsl/ComposableXmlDeclarationTestCase.java
@@ -9,13 +9,14 @@ package org.mule.runtime.extension.api.dsl;
 import static java.util.Arrays.asList;
 import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mule.runtime.api.util.ExtensionModelTestUtils.visitableMock;
+import static org.mule.runtime.api.util.NameUtils.hyphenize;
 
 import org.mule.runtime.api.meta.ExpressionSupport;
 import org.mule.runtime.api.meta.model.ParameterDslConfiguration;
@@ -27,6 +28,7 @@ import org.mule.runtime.extension.api.dsl.syntax.DslElementSyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.junit.Before;
@@ -36,6 +38,9 @@ import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
 public class ComposableXmlDeclarationTestCase extends BaseXmlDeclarationTestCase {
+
+  private static final String INLINE_PARAMETER_NAME = "inlineParameterName";
+  private static final String PARAMETER_GROUP_SHOW_IN_DSL_NAME = "InlineParameterGroup";
 
   private NestedComponentModel firstNestedComponentModel = mock(NestedComponentModel.class);
   private NestedComponentModel secondNestedComponentModel = mock(NestedComponentModel.class);
@@ -85,17 +90,17 @@ public class ComposableXmlDeclarationTestCase extends BaseXmlDeclarationTestCase
 
     ParameterModel parameterModelMock = mock(ParameterModel.class);
     ParameterGroupModel parameterGroupModelMock = mock(ParameterGroupModel.class);
-    when(parameterModelMock.getName()).thenReturn("inlineParameterName");
+    when(parameterModelMock.getName()).thenReturn(INLINE_PARAMETER_NAME);
     when(parameterModelMock.getType()).thenReturn(TYPE_LOADER.load(String.class));
     when(parameterModelMock.getExpressionSupport()).thenReturn(ExpressionSupport.SUPPORTED);
     when(parameterModelMock.getModelProperty(any())).thenReturn(empty());
     when(parameterModelMock.getDslConfiguration()).thenReturn(ParameterDslConfiguration.getDefaultInstance());
     when(parameterModelMock.getLayoutModel()).thenReturn(empty());
-    when(parameterModelMock.getRole()).thenReturn(role);
+    when(parameterModelMock.getRole()).thenReturn(ParameterRole.BEHAVIOUR);
 
-    when(parameterGroupModelMock.getName()).thenReturn("InlineParameterGroup");
+    when(parameterGroupModelMock.getName()).thenReturn(PARAMETER_GROUP_SHOW_IN_DSL_NAME);
     when(parameterGroupModelMock.isShowInDsl()).thenReturn(true);
-    when(parameterGroupModelMock.getParameterModels()).thenReturn(asList(parameterModel));
+    when(parameterGroupModelMock.getParameterModels()).thenReturn(asList(parameterModelMock));
 
 
     when(innerNestedComponentModel.getParameterGroupModels()).thenReturn(asList(parameterGroupModelMock));
@@ -124,17 +129,48 @@ public class ComposableXmlDeclarationTestCase extends BaseXmlDeclarationTestCase
   @Test
   public void innerNestedComposableComponent() {
     DslElementSyntax result = getSyntaxResolver().resolve(operation);
+    Optional<DslElementSyntax> innerNested =
+        result.getChild(firstNestedComponentModel.getName()).get().getChild(innerNestedComponentModel.getName());
+    assertThat(innerNested.isPresent(), is(true));
+    assertThat(innerNested.get().getElementName(), is(innerNestedComponentModel.getName()));
   }
 
   @Test
   public void nestedComposableComponentParameters() {
     DslElementSyntax result = getSyntaxResolver().resolve(operation);
+    List<DslElementSyntax> firstNestedComponentChildren = new ArrayList<>();
+    firstNestedComponentChildren.add(result.getChild(firstNestedComponentModel.getName()).get());
+    firstNestedComponentChildren.add(result.getChild(secondNestedComponentModel.getName()).get());
+    firstNestedComponentChildren.add(result.getChild(thirdNestedComponentModel.getName()).get());
+
+    for (DslElementSyntax child : firstNestedComponentChildren) {
+      if (parameterModel.getRole().equals(ParameterRole.BEHAVIOUR)) {
+        Optional<DslElementSyntax> parameterDslSyntax = child.getAttribute(parameterModel.getName());
+        assertThat(parameterDslSyntax.isPresent(), is(true));
+        assertThat(parameterDslSyntax.get().getAttributeName(), is((parameterModel.getName())));
+      } else {
+        Optional<DslElementSyntax> parameterDslSyntax = child.getChild(parameterModel.getName());
+        assertThat(parameterDslSyntax.isPresent(), is(true));
+        assertThat(parameterDslSyntax.get().getElementName(), is((hyphenize(parameterModel.getName()))));
+      }
+    }
   }
 
   @Test
   public void innerNestedCompasableComponentInlineParameter() {
     DslElementSyntax result = getSyntaxResolver().resolve(operation);
+    Optional<DslElementSyntax> firstNestedComponentModelSyntax = result.getChild(firstNestedComponentModel.getName());
+    assertThat(firstNestedComponentModelSyntax.isPresent(), is(true));
+    Optional<DslElementSyntax> innerNestedComponentModelSyntax =
+        firstNestedComponentModelSyntax.get().getChild(innerNestedComponentModel.getName());
+    assertThat(innerNestedComponentModelSyntax.isPresent(), is(true));
+    Optional<DslElementSyntax> inlineParameterGroupSyntax =
+        innerNestedComponentModelSyntax.get().getChild(PARAMETER_GROUP_SHOW_IN_DSL_NAME);
+    assertThat(inlineParameterGroupSyntax.isPresent(), is(true));
+    Optional<DslElementSyntax> parameterInInlineGroupSyntax =
+        inlineParameterGroupSyntax.get().getAttribute(INLINE_PARAMETER_NAME);
+    assertThat(parameterInInlineGroupSyntax.isPresent(), is(true));
+    assertThat(parameterInInlineGroupSyntax.get().getAttributeName(), is(INLINE_PARAMETER_NAME));
   }
-
 
 }
