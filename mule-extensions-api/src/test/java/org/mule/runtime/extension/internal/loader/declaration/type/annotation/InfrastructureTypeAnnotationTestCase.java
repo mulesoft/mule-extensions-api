f075c8590392a3c2e9b91a8c7eeefca40a857e28
ndinu <ndinu@users.noreply.github.com>
2021-08-23 09:08:06 -0300

    MULE-19726: Error resolving syntax for infrastructure type (#845)

---
+++ mule-extensions-api/src/test/java/org/mule/runtime/extension/internal/loader/declaration/type/annotation/InfrastructureTypeAnnotationTestCase.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.internal.loader.declaration.type.annotation;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.mule.runtime.extension.api.declaration.type.annotation.InfrastructureTypeAnnotation;
+
+import io.qameta.allure.Issue;
+import org.junit.Test;
+
+@Issue("MULE-19726")
+public class InfrastructureTypeAnnotationTestCase {
+
+  @Test
+  public void infrastructureTypeAnnotationIsPublic() {
+    InfrastructureTypeAnnotation infrastructureTypeAnnotation = new InfrastructureTypeAnnotation();
+    assertThat(infrastructureTypeAnnotation.isPublic(), is(true));
+  }
+
+}
