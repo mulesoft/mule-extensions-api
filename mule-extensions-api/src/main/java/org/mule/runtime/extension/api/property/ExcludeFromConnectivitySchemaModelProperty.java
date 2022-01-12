0ec82d5c54a771ae240e2d68949e9267b68a6703
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2022-01-12 09:52:35 -0300

    MULE-20030: Remove connectivity-api from Mule Runtime (#890)

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/property/ExcludeFromConnectivitySchemaModelProperty.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.property;
+
+import org.mule.runtime.api.meta.model.ModelProperty;
+
+/**
+ * A {@link ModelProperty} which indicates that the enriched element should not be a part of any generated Connectivity Schema
+ *
+ * @since 1.5.0
+ */
+public class ExcludeFromConnectivitySchemaModelProperty implements ModelProperty {
+
+  @Override
+  public String getName() {
+    return "ExcludeFromConnectivitySchema";
+  }
+
+  @Override
+  public boolean isPublic() {
+    return false;
+  }
+}
