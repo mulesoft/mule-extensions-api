b78aed3e2927297c558434d868295959d150c94b
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2023-01-18 13:40:03 -0300

    W-12389157: Support MessageSources in ExtensionsClient (#947)

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/client/source/SourceHandler.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.client.source;
+
+import org.mule.api.annotation.Experimental;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.lifecycle.Disposable;
+import org.mule.runtime.api.lifecycle.Startable;
+import org.mule.runtime.api.lifecycle.Stoppable;
+import org.mule.runtime.extension.api.client.ExtensionsClient;
+import org.mule.sdk.api.annotation.MinMuleVersion;
+
+/**
+ * Offers management and lifecycle control over a message source created through the {@link ExtensionsClient}.
+ * <p>
+ * <b>NOTE:</b> Experimental feature. Backwards compatibility not guaranteed.
+ *
+ * @since 1.6.0
+ */
+@Experimental
+@MinMuleVersion("4.6.0")
+public interface SourceHandler extends Startable, Stoppable, Disposable {
+
+  /**
+   * Starts the source, which means it will start producing messages
+   * 
+   * @throws MuleException if it fails to start
+   */
+  @Override
+  void start() throws MuleException;
+
+  /**
+   * Stops the source, which means it will stop producing messages. It can be restarted by invoking {@link #start()} again
+   * 
+   * @throws MuleException if it fails to stop
+   */
+  @Override
+  void stop() throws MuleException;
+
+  /**
+   * Completely disposes the source and all its allocated resources. It can't be restarted, a new instance needs to be created.
+   */
+  @Override
+  void dispose();
+}
---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/client/source/SourceParameterizer.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.client.source;
+
+import org.mule.api.annotation.Experimental;
+import org.mule.runtime.extension.api.client.ExtensionsClient;
+import org.mule.runtime.extension.api.client.params.ComponentParameterizer;
+import org.mule.runtime.extension.api.runtime.source.BackPressureMode;
+import org.mule.sdk.api.annotation.MinMuleVersion;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Parameterizes a source created through the {@link ExtensionsClient}
+ * <p>
+ * <b>NOTE:</b> Experimental feature. Backwards compatibility not guaranteed.
+ *
+ * @since 1.6.0
+ */
+@Experimental
+@MinMuleVersion("4.6.0")
+public interface SourceParameterizer extends ComponentParameterizer<SourceParameterizer> {
+
+  /**
+   * Sets the {@link BackPressureMode} for the source
+   *
+   * @param backPressureMode the back pressure mode
+   * @return {@code this} instance
+   */
+  SourceParameterizer withBackPressureMode(BackPressureMode backPressureMode);
+
+  /**
+   * Specifies a fixed frequency scheduling strategy. Only invoke for polling based sources which specify a scheduling strategy
+   * parameter.
+   *
+   * @param frequency  the executing frequency
+   * @param timeUnit   the {@code frequency} {@link TimeUnit}
+   * @param startDelay the start delay
+   * @return {@code this} instance
+   */
+  SourceParameterizer withFixedSchedulingStrategy(long frequency, TimeUnit timeUnit, long startDelay);
+
+  /**
+   * Specifies a cron expression scheduling strategy. Only invoke for polling based sources which specify a scheduling strategy
+   * parameter.
+   *
+   * @param expression the cron expression
+   * @param timeZone   the cron timezone
+   * @return {@code this} instance
+   */
+  SourceParameterizer withCronSchedulingStrategy(String expression, String timeZone);
+
+}
