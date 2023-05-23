80558f6f0562f8afab583792146d853720fddb72
elrodro83 <rmerino@salesforce.com>
2023-05-23 17:40:33 -0300

    W-13312344: JPMS - Cannot load services using a utils in a module when the interface is in another module

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/spi/ExtensionsApiSpiUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.internal.spi;
+
+import static java.util.ServiceLoader.load;
+import static java.util.stream.StreamSupport.stream;
+
+import org.mule.runtime.extension.api.dsl.syntax.resources.spi.DslResourceFactory;
+import org.mule.runtime.extension.api.dsl.syntax.resources.spi.ExtensionSchemaGenerator;
+import org.mule.runtime.extension.api.loader.ExtensionModelLoaderProvider;
+import org.mule.runtime.extension.api.resources.spi.GeneratedResourceFactory;
+
+import java.util.stream.Stream;
+
+/**
+ * Provides utilities to lookup and load implementations of interfaces defined in {@code extensions-api} through SPI.
+ * 
+ * @since 1.6
+ */
+public final class ExtensionsApiSpiUtils {
+
+  private ExtensionsApiSpiUtils() {
+    // Nothing to do
+  }
+
+  public static Stream<ExtensionModelLoaderProvider> loadExtensionModelLoaderProviders(ClassLoader classLoader) {
+    return stream(((Iterable<ExtensionModelLoaderProvider>) () -> load(ExtensionModelLoaderProvider.class, classLoader)
+        .iterator())
+            .spliterator(),
+                  false);
+  }
+
+  public static Stream<ExtensionSchemaGenerator> loadExtensionSchemaGenerators(ClassLoader classLoader) {
+    return stream(((Iterable<ExtensionSchemaGenerator>) () -> load(ExtensionSchemaGenerator.class, classLoader)
+        .iterator())
+            .spliterator(),
+                  false);
+  }
+
+  public static Stream<DslResourceFactory> loadDslResourceFactories(ClassLoader classLoader) {
+    return stream(((Iterable<DslResourceFactory>) () -> load(DslResourceFactory.class, classLoader)
+        .iterator())
+            .spliterator(),
+                  false);
+  }
+
+  public static Stream<GeneratedResourceFactory> loadGeneratedResourceFactories(ClassLoader classLoader) {
+    return stream(((Iterable<GeneratedResourceFactory>) () -> load(GeneratedResourceFactory.class, classLoader)
+        .iterator())
+            .spliterator(),
+                  false);
+  }
+
+}
