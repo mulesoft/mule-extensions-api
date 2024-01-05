00bd51727bbba5b23ac6108649ba1bf5bf7b4732
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2024-01-05 11:15:21 -0300

    draft

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/api/dsl/syntax/XmlDslSyntaxResolver.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/dsl/syntax/XmlDslSyntaxResolver.java
@@ -343,9 +343,16 @@ public class XmlDslSyntaxResolver implements DslSyntaxResolver {
     boolean supportTopLevelElement = supportTopLevelElement(type);
 
     boolean supportsInlineDeclaration = supportsInlineDeclaration(type, NOT_SUPPORTED);
-    if (!supportsInlineDeclaration && !type.getAnnotation(TypeDslAnnotation.class).isPresent()) {
-      supportsInlineDeclaration = isSubtype && isInstantiable(type);
+
+    if (!supportsInlineDeclaration && isSubtype) {
+      supportsInlineDeclaration = type.getAnnotation(TypeDslAnnotation.class)
+          .map(TypeDslAnnotation::allowsInlineDefinition)
+          .orElseGet(() -> isInstantiable(type));
     }
+
+    // if (!supportsInlineDeclaration && !type.getAnnotation(TypeDslAnnotation.class).isPresent()) {
+    // supportsInlineDeclaration = isSubtype && isInstantiable(type);
+    // }
 
     if (!supportsInlineDeclaration && !supportTopLevelElement && !requiresWrapper && !isSubtype) {
       return empty();
