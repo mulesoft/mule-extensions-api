75dcafee30eb4546bedc261e0913a3333f90d34e
Martín Buchwald <martin.buchwald@mulesoft.com>
2024-04-22 11:02:47 -0300

    W-15261626: Set OutputTypeResolvers to Core's Scope and Routers (#1092)

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/metadata/DefaultComponentMetadataConfigurerFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.internal.metadata;
+
+import static java.util.ServiceLoader.load;
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.api.util.LazyValue;
+import org.mule.runtime.extension.api.metadata.ComponentMetadataConfigurer;
+import org.mule.runtime.extension.api.metadata.ComponentMetadataConfigurerFactory;
+
+import java.util.Iterator;
+
+public class DefaultComponentMetadataConfigurerFactory extends ComponentMetadataConfigurerFactory {
+
+  private static final LazyValue<ComponentMetadataConfigurerFactoryDelegate> DELEGATE =
+      new LazyValue(() -> {
+        Iterator<ComponentMetadataConfigurerFactoryDelegate> iter =
+            load(ComponentMetadataConfigurerFactoryDelegate.class,
+                 ComponentMetadataConfigurerFactoryDelegate.class.getClassLoader()).iterator();
+        if (!iter.hasNext()) {
+          throw new MuleRuntimeException(createStaticMessage("There is no implementation available for %s.",
+                                                             ComponentMetadataConfigurerFactoryDelegate.class.getName()));
+        }
+        return iter.next();
+      });
+
+  @Override
+  public ComponentMetadataConfigurer create() {
+    ComponentMetadataConfigurerFactoryDelegate delegate = DELEGATE.get();
+    return delegate.create();
+  }
+}
