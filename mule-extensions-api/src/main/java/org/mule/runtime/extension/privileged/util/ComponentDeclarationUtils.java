c2630ed78c3b6878edcf31b6c4696d4dd78de845
elrodro83 <rmerino@salesforce.com>
2024-05-20 10:04:08 -0300

    more review

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/privileged/util/ComponentDeclarationUtils.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/privileged/util/ComponentDeclarationUtils.java
@@ -13,6 +13,8 @@ import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclarer;
 import org.mule.runtime.api.meta.model.declaration.fluent.OperationDeclarer;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterModel;
+import org.mule.runtime.api.tx.TransactionType;
+import org.mule.runtime.extension.api.ExtensionConstants;
 import org.mule.runtime.extension.internal.property.NoConnectivityErrorModelProperty;
 import org.mule.runtime.extension.internal.property.NoErrorMappingModelProperty;
 import org.mule.runtime.extension.internal.property.NoReconnectionStrategyModelProperty;
@@ -38,59 +40,127 @@ public final class ComponentDeclarationUtils {
     // nothing to do
   }
 
+  /**
+   * Marks that the operation being declared by the {@code declarer} may not have a configuring streaming strategy, regardless of
+   * it being a streaming operation.
+   * 
+   * @param declarer the declarer of the operation to mark.
+   */
   public static final void withNoStreamingConfiguration(OperationDeclarer declarer) {
     declarer.withModelProperty(new NoStreamingConfigurationModelProperty());
   }
 
+  /**
+   * Marks that the operation being declared by the {@code declarer} may not have a configuring transactionalAction, regardless of
+   * being transactional.
+   * 
+   * @param declarer the declarer of the operation to mark.
+   */
   public static final void withNoTransactionalAction(OperationDeclarer declarer) {
     declarer.withModelProperty(new NoTransactionalActionModelProperty());
   }
 
+  /**
+   * Marks that the operation being declared by the {@code declarer} may not have a
+   * {@link ExtensionConstants#RECONNECTION_CONFIG_PARAMETER_NAME} parameter, regardless of being connected.
+   * 
+   * @param declarer the declarer of the operation to mark.
+   */
   public static final void withNoReconnectionStrategy(OperationDeclarer declarer) {
     declarer.withModelProperty(new NoReconnectionStrategyModelProperty());
   }
 
+  /**
+   * Marks that the operations of the extension being declared by the {@code declarer} may not have a
+   * {@link ExtensionConstants#RECONNECTION_CONFIG_PARAMETER_NAME} parameter, regardless of them being connected.
+   * 
+   * @param declarer the declarer of the extension to mark.
+   */
   // account for use cases where this is declared at the extension level for all its operations
   public static final void withNoReconnectionStrategy(ExtensionDeclarer declarer) {
     declarer.withModelProperty(new NoReconnectionStrategyModelProperty());
   }
 
+  /**
+   * Marks that the operation being declared by the {@code declarer} will avoid propagating connectivity errors from operations,
+   * regardless of being connected.
+   * 
+   * @param declarer the declarer of the operation to mark.
+   */
   public static final void withNoConnectivityError(OperationDeclarer declarer) {
     declarer.withModelProperty(new NoConnectivityErrorModelProperty());
   }
 
+  /**
+   * Marks that the operation being declared by the {@code declarer} may not contain errorMappings.
+   * 
+   * @param declarer the declarer of the operation to mark.
+   */
   public static final void withNoErrorMapping(OperationDeclarer declarer) {
     declarer.withModelProperty(new NoErrorMappingModelProperty());
   }
 
+  /**
+   * Marks that the component being declared by the {@code declarer} is paged.
+   * 
+   * @param declarer the declarer of the component to mark.
+   */
   public static final void asPagedOperation(ComponentDeclarer declarer) {
     declarer.withModelProperty(new PagedOperationModelProperty());
   }
 
+  /**
+   * @param componentModel the component to check for.
+   * @return whether {@code componentModel} may not have a configuring transactionalAction.
+   */
   public static final boolean isNoTransactionalAction(ComponentModel componentModel) {
     return componentModel.getModelProperty(NoTransactionalActionModelProperty.class).isPresent();
   }
 
+  /**
+   * @param componentModel the operation to check for.
+   * @return whether {@code componentModel} may not contain errorMappings.
+   */
   public static final boolean isNoErrorMapping(OperationModel componentModel) {
     return componentModel.getModelProperty(NoErrorMappingModelProperty.class).isPresent();
   }
 
+  /**
+   * @param componentModel the component to check for.
+   * @return whether {@code componentModel} is paged.
+   */
   public static final boolean isPagedOperation(ComponentModel componentModel) {
     return componentModel.getModelProperty(PagedOperationModelProperty.class).isPresent();
   }
 
-  public static final boolean hasTransactionalAction(ParameterModel paramModel) {
+  /**
+   * @param paramModel the parameter to check for.
+   * @return whether {@code paramModel} is the parameter considered to inject the value of the Transactional Action.
+   */
+  public static final boolean isTransactionalAction(ParameterModel paramModel) {
     return paramModel.getModelProperty(TransactionalActionModelProperty.class).isPresent();
   }
 
-  public static final boolean hasTransactionalType(ParameterModel paramModel) {
+  /**
+   * @param paramModel the parameter to check for.
+   * @return whether {@code paramModel} is the parameter is a {@link TransactionType}.
+   */
+  public static final boolean isTransactionalType(ParameterModel paramModel) {
     return paramModel.getModelProperty(TransactionalTypeModelProperty.class).isPresent();
   }
 
+  /**
+   * @param paramModelProperties the parameter to check for.
+   * @return whether the paramModel owning the provided {@code paramModelProperties} is the parameter considered as a Target type.
+   */
   public static boolean isTargetParameter(Set<ModelProperty> paramModelProperties) {
     return paramModelProperties.stream().anyMatch(modelProperty -> modelProperty instanceof TargetModelProperty);
   }
 
+  /**
+   * @param paramModel the parameter to check for.
+   * @return whether {@code paramModel} is the parameter considered as a Target type.
+   */
   public static boolean isTargetParameter(ParameterModel paramModel) {
     return isTargetParameter(paramModel.getModelProperties());
   }
