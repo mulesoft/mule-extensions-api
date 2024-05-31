dc7fbde8232620890bf2087279d4c84cd17de406
rbourbonnavarro <rodrigobourbon44@gmail.com>
2024-05-31 09:52:47 -0300

    Address usage of `JavaParserUtils`. (#1117)

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/loader/util/JavaParserUtils.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/loader/util/JavaParserUtils.java
@@ -11,7 +11,6 @@ import static java.util.Optional.ofNullable;
 
 import org.mule.runtime.api.meta.Category;
 import org.mule.runtime.api.meta.ExpressionSupport;
-import org.mule.runtime.api.meta.ExternalLibraryType;
 import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.param.ConfigOverride;
@@ -25,8 +24,6 @@ import java.lang.reflect.Field;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
-
-import javax.lang.model.element.Element;
 
 /**
  * Utilities for parsing Extensions defined through the Java language.
@@ -48,7 +45,7 @@ public final class JavaParserUtils {
    * @return the field's alias, as defined by any of the {@code @Alias} annotations
    */
   public static String getAlias(Field field) {
-    return getAlias(field, field::getName);
+    return getAlias(field::getAnnotation, field::getName);
   }
 
   /**
@@ -56,7 +53,7 @@ public final class JavaParserUtils {
    * @return the class alias, as defined by any of the {@code @Alias} annotations
    */
   public static String getAlias(Class<?> clazz) {
-    return getAlias(clazz, clazz::getSimpleName);
+    return getAlias(clazz::getAnnotation, clazz::getSimpleName);
   }
 
   /**
@@ -79,8 +76,8 @@ public final class JavaParserUtils {
    * @param defaultValue     a default value supplier
    * @return the resolved alias
    */
-  public static String getAlias(Function<Class<? extends Annotation>, Annotation> annotationMapper,
-                                Supplier<String> defaultValue) {
+  private static String getAlias(Function<Class<? extends Annotation>, Annotation> annotationMapper,
+                                 Supplier<String> defaultValue) {
     String name = null;
     Alias legacyAlias = (Alias) annotationMapper.apply(Alias.class);
     if (legacyAlias != null) {
@@ -101,15 +98,7 @@ public final class JavaParserUtils {
    * @return the {@link ExpressionSupport} defined for the element, if defined. {@link Optional#empty()} otherwise.
    */
   public static Optional<ExpressionSupport> getExpressionSupport(AnnotatedElement element) {
-    return getExpressionSupport(element::getAnnotation);
-  }
-
-  /**
-   * @param mapper function which encapsulates annotation resolution
-   * @return the {@link ExpressionSupport} defined for the element, if defined. {@link Optional#empty()} otherwise.
-   */
-  public static Optional<ExpressionSupport> getExpressionSupport(Function<Class<? extends Annotation>, ? extends Annotation> mapper) {
-    return mapReduceAnnotation(mapper,
+    return mapReduceAnnotation(element::getAnnotation,
                                Expression.class,
                                org.mule.sdk.api.annotation.Expression.class,
                                ann -> ann.value(),
@@ -145,12 +134,12 @@ public final class JavaParserUtils {
   }
 
   /**
-   * Monad for extracting information from an {@link Element} which might be annotated with two different annotations of similar
-   * semantics. Both annotations types are reduced to a single output type.
+   * Monad for extracting information from a {@link Class} which might be annotated with two different annotations of similar
+   * semantics. Both annotations' types are reduced to a single output type.
    * <p>
    * Simultaneous presence of both types will be considered an error
    *
-   * @param element                 the annotated element
+   * @param type                    the annotated class
    * @param legacyAnnotationClass   the legacy annotation type
    * @param sdkAnnotationClass      the new annotation type
    * @param legacyAnnotationMapping mapping function for the legacy annotation
@@ -160,20 +149,6 @@ public final class JavaParserUtils {
    * @param <T>                     Output generic type
    * @return a reduced value
    */
-  public static <R extends Annotation, S extends Annotation, T> Optional<T> mapReduceAnnotation(
-                                                                                                Element element,
-                                                                                                Class<R> legacyAnnotationClass,
-                                                                                                Class<S> sdkAnnotationClass,
-                                                                                                Function<R, T> legacyAnnotationMapping,
-                                                                                                Function<S, T> sdkAnnotationMapping) {
-
-    return mapReduceAnnotation(element::getAnnotation,
-                               legacyAnnotationClass,
-                               sdkAnnotationClass,
-                               legacyAnnotationMapping,
-                               sdkAnnotationMapping);
-  }
-
   public static <R extends Annotation, S extends Annotation, T> Optional<T> mapReduceAnnotation(
                                                                                                 Class<?> type,
                                                                                                 Class<R> legacyAnnotationClass,
@@ -225,25 +200,6 @@ public final class JavaParserUtils {
       return Category.PREMIUM;
     } else {
       throw new IllegalModelDefinitionException("Unsupported Category type " + category);
-    }
-  }
-
-  /**
-   * Transforms an sdk-api {@link org.mule.sdk.api.meta.ExternalLibraryType} into a mule-api {@link ExternalLibraryType}
-   *
-   * @param type an sdk-api representation of the library type semantic
-   * @return the transformed value
-   * @throws IllegalModelDefinitionException if no equivalent semantic found.
-   */
-  public static ExternalLibraryType toMuleApi(org.mule.sdk.api.meta.ExternalLibraryType type) {
-    if (type == org.mule.sdk.api.meta.ExternalLibraryType.JAR) {
-      return ExternalLibraryType.JAR;
-    } else if (type == org.mule.sdk.api.meta.ExternalLibraryType.DEPENDENCY) {
-      return ExternalLibraryType.DEPENDENCY;
-    } else if (type == org.mule.sdk.api.meta.ExternalLibraryType.NATIVE) {
-      return ExternalLibraryType.NATIVE;
-    } else {
-      throw new IllegalModelDefinitionException("Unsupported ExternalLIbraryType " + type);
     }
   }
 
