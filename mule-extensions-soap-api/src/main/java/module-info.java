9e12e5ac6797a7a4dbe591ad3f90afb2ccf59639
elrodro83 <rmerino@salesforce.com>
2024-10-01 15:46:24 -0300

    Remove soap Service

--- mule-extensions-soap-api/src/main/java/module-info.java
+++ mule-extensions-soap-api/src/main/java/module-info.java
@@ -12,18 +12,4 @@
  */
 module org.mule.runtime.extensions.soap.api {
 
-  requires org.mule.runtime.api;
-  requires org.mule.runtime.extensions.api;
-  requires org.mule.runtime.metadata.model.api;
-
-  requires org.apache.commons.io;
-  requires com.google.common;
-
-  exports org.mule.runtime.extension.api.soap;
-  exports org.mule.runtime.extension.api.soap.message;
-  exports org.mule.runtime.extension.api.soap.metadata;
-  exports org.mule.runtime.extension.api.soap.security;
-  exports org.mule.runtime.extension.api.soap.security.config;
-  exports org.mule.runtime.extension.api.soap.annotation;
-
 }
