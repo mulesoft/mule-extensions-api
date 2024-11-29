78b1cd1f68e630603371010360d384374e3aac3b
Rodrigo Merino <elrodro83@users.noreply.github.com>
2024-11-29 09:55:33 -0300

    W-17106906: Add TlsContextFactory as an imported type to ExtensionModel (#1191)

--- mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/loader/enricher/ConnectionProviderDeclarationEnricher.java
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/internal/loader/enricher/ConnectionProviderDeclarationEnricher.java
@@ -6,17 +6,26 @@
  */
 package org.mule.runtime.extension.internal.loader.enricher;
 
-import static java.util.Optional.empty;
+import static org.mule.metadata.api.utils.MetadataTypeUtils.getTypeId;
 import static org.mule.runtime.api.meta.model.connection.ConnectionManagementType.POOLING;
 import static org.mule.runtime.extension.api.loader.DeclarationEnricherPhase.STRUCTURE;
 import static org.mule.runtime.extension.internal.loader.util.InfrastructureParameterBuilder.addPoolingProfileParameter;
 import static org.mule.runtime.extension.internal.loader.util.InfrastructureParameterBuilder.addReconnectionConfigParameter;
 import static org.mule.runtime.extension.privileged.util.ComponentDeclarationUtils.isReconnectionStrategySupported;
 
+import static java.util.Optional.of;
+
+import org.mule.metadata.api.ClassTypeLoader;
+import org.mule.metadata.api.model.MetadataType;
+import org.mule.metadata.api.model.ObjectType;
 import org.mule.runtime.api.config.PoolingProfile;
+import org.mule.runtime.api.meta.model.ImportedTypeModel;
 import org.mule.runtime.api.meta.model.connection.ConnectionManagementType;
 import org.mule.runtime.api.meta.model.declaration.fluent.ConnectionProviderDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclarer;
+import org.mule.runtime.api.tls.TlsContextFactory;
+import org.mule.runtime.extension.api.declaration.type.ExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.loader.DeclarationEnricherPhase;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.extension.api.loader.IdempotentDeclarationEnricherWalkDelegate;
@@ -49,24 +58,49 @@ public class ConnectionProviderDeclarationEnricher implements WalkingDeclaration
 
   @Override
   public Optional<DeclarationEnricherWalkDelegate> getWalkDelegate(ExtensionLoadingContext extensionLoadingContext) {
-    final ExtensionDeclaration declaration = extensionLoadingContext.getExtensionDeclarer().getDeclaration();
+    final ExtensionDeclarer extensionDeclarer = extensionLoadingContext.getExtensionDeclarer();
+    final ExtensionDeclaration extDeclaration = extensionDeclarer.getDeclaration();
 
-    if (isReconnectionStrategySupported(declaration)) {
-      return Optional.of(new IdempotentDeclarationEnricherWalkDelegate() {
+    return of(new IdempotentDeclarationEnricherWalkDelegate() {
 
-        @Override
-        protected void onConnectionProvider(ConnectionProviderDeclaration declaration) {
+      boolean thereAreConnectionProviders = false;
+
+      @Override
+      protected void onConnectionProvider(ConnectionProviderDeclaration declaration) {
+        if (isReconnectionStrategySupported(extDeclaration)) {
           addReconnectionConfigParameter(declaration);
-          ConnectionManagementType managementType = declaration.getConnectionManagementType();
-
-          if (managementType == POOLING) {
-            addPoolingProfileParameter(declaration);
-          }
         }
-      });
-    }
+        ConnectionManagementType managementType = declaration.getConnectionManagementType();
 
-    return empty();
+        if (managementType == POOLING) {
+          addPoolingProfileParameter(declaration);
+        }
+
+        thereAreConnectionProviders = true;
+      }
+
+      @Override
+      public void onWalkFinished() {
+        if (thereAreConnectionProviders && !isTlsContextFactoryAlreadyImported(extDeclaration)) {
+          ClassTypeLoader typeLoader = ExtensionsTypeLoaderFactory.getDefault().createTypeLoader();
+          extensionDeclarer.withImportedType(new ImportedTypeModel((ObjectType) loadTlsContextFactoryType(typeLoader)));
+        }
+      }
+    });
+  }
+
+  private MetadataType loadTlsContextFactoryType(ClassTypeLoader loader) {
+    return loader.load(TlsContextFactory.class);
+  }
+
+  private boolean isTlsContextFactoryAlreadyImported(ExtensionDeclaration extension) {
+    return extension.getImportedTypes().stream().anyMatch(model -> isTlsContextFactory(model.getImportedType()));
+  }
+
+  private boolean isTlsContextFactory(MetadataType type) {
+    return getTypeId(type)
+        .filter(typeId -> TlsContextFactory.class.getName().equals(typeId))
+        .isPresent();
   }
 
 }
