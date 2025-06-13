ade126e385cb36355394c2638cfa1be6fc9b3b0f
Axel Sanguinetti <asanguinetti@salesforce.com>
2025-06-13 15:52:46 -0300

    W-18516804: Promoting ExtensionModelParsers API to extensions-api

---
+++ mule-extensions-api/src/main/java/org/mule/runtime/extension/api/loader/parser/ComponentVisibilityParser.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.api.loader.parser;
+
+import org.mule.runtime.api.meta.model.ComponentVisibility;
+
+/**
+ * General contract for a model parser capable of reading component visibility
+ *
+ * @since 1.10.0
+ */
+public interface ComponentVisibilityParser {
+
+  /**
+   * @return the operation's {@link ComponentVisibility}.
+   */
+  ComponentVisibility getComponentVisibility();
+}
